d4360edde7657f20a62923a2abb9c4d8c31335f4
Abhay Mishra <techabhay06@gmail.com>
2024-11-17 01:50:37 +0530

    Add new commit

--- App.java
+++ App.java
@@ -1,7 +1,6 @@
 
-       import java.awt.event.*;
-
-import java.awt.*;
+       import java.awt.*;
+import java.awt.event.*;
 
 class sketch_pad extends Frame implements ActionListener,WindowListener,MouseListener,MouseMotionListener,ItemListener
 
@@ -38,6 +37,156 @@ class sketch_pad extends Frame implements ActionListener,WindowListener,MouseLis
     set_menu_items();
     setBackground(Color.white);
   }
+
+  public void actionPerformed(ActionEvent action_performed_eve)
+  {
+    Graphics ga = getGraphics();
+    Object s = action_performed_eve.getActionCommand();
+    for (int i=0; i != color_list.length; i++)
+      if (s.equals(color_list[i]))
+      {
+        selected_color = color_list[i];
+        return ;
+      }
+    
+    for (int i=0; i != shape_list.length; i++)
+         if (s.equals(shape_list[i]))
+         {
+            selected_shape = shape_list[i];
+            return;
+         }
+
+    if (s.equals("Eraser"))
+    {
+        Eraser = true;
+        return;
+    }
+
+    else if (s.equals("Clear Canvas"))
+    {
+        ga.clearRect(0, 0, 700, 700);
+        return;
+    }
+  }
+
+  void choose_color(Graphics ga)
+  {
+    for (int i = 0; i != color_list.length; i++)
+    {
+        if (selected_color.equals(color_list[i]))
+        {
+            switch (i)
+            {
+                case 0: ga.setColor(Color.black); break;
+                case 1: ga.setColor(Color.cyan); break;
+                case 2: ga.setColor(Color.green); break;
+                case 3: ga.setColor(Color.yellow); break;
+                case 4: ga.setColor(Color.magenta); break;
+                case 5: ga.setColor(Color.red); break;
+                case 6: ga.setColor(Color.blue);
+            }
+        }
+    }
+  }
+
+  public void mouseReleased (MouseEvent mouse_reles_eve)
+  {
+    Graphics ga = getGraphics();
+    if (Eraser)
+    {
+        Eraser = false;
+        return;
+    }
+    choose_color(ga);
+    sel_x2= mouse_reles_eve.getX();
+    sel_y2= mouse_reles_eve.getY();
+
+    if (selected_shape.equals("Line"))
+    {
+        ga.drawLine(sel_xl, sel_yl, sel_x2, sel_y2);
+    }
+    else if (selected_shape.equals("Circle"))
+    {
+        draw_selected_shape (ga, "Circle");
+    }
+    else if (selected_shape.equals("Square"))
+    {
+        draw_selected_shape (ga, "Square");
+    }
+    else if (selected_shape.equals("Rectangle"))
+    {
+        draw_selected_shape (ga, "Rectangle");
+    }
+
+    ga.setColor(Color.yellow);  
+    ga.drawString(".", sel_xl, sel_yl);
+    ga.setColor(Color.black);
+
+  }
+
+  void draw_selected_shape (Graphics ga, String sel_shape)
+
+  {
+    up_L_X = Math.min(sel_xl, sel_x2);
+    up_L_Y = Math.min(sel_yl, sel_y2);
+    W = Math.abs(sel_xl-sel_x2);
+    H = Math.abs(sel_yl-sel_y2);
+
+    if (sel_shape.equals("Square")  )
+      ga.fillRect(up_L_X, up_L_Y, W,W);
+      else if (sel_shape.equals("Rectangle") )
+      ga.fillRect(up_L_X, up_L_Y, W, H);
+    else if (sel_shape.equals("Circle")  )
+      ga.fillOval(up_L_X, up_L_Y, W, W);
+
+  }
+
+  public void mouseDragged (MouseEvent mouse_drag_eve)
+  {
+    Graphics ga = getGraphics();
+    sel_x2 = mouse_drag_eve.getX();
+    sel_y2 = mouse_drag_eve.getY();
+
+    if (Eraser)
+    {
+        ga.setColor(Color.white);
+        ga.fillRect(sel_x2, sel_y2, 10, 10);
+    }
+  }
+
+  public void mousePressed (MouseEvent mouse_press_eve)
+  {
+    if (Eraser)  return ;
+    up_L_X=0; up_L_Y=0; W=0; H=0;
+    sel_xl = mouse_press_eve.getX();
+    sel_yl = mouse_press_eve.getY();
+    Graphics ga = getGraphics();
+    ga.drawString(".", sel_xl, sel_yl);
+  }
+
+void set_menu_items()
+  {
+    MenuBar mBar = new MenuBar();
+
+      Menu menu_sh = new Menu ("Shapes");
+    for (int i = 0; i != shape_list.length; i++)
+      menu_sh.add(shape_list[i]);
+    mBar.add(menu_sh);
+    menu_sh.addActionListener(this);
+
+      Menu menu_col = new Menu ("Colors");
+    for (int i =0; i != color_list.length; i++)
+      menu_col.add(color_list[i]);
+    mBar.add(menu_col);
+    menu_col.addActionListener(this);
+
+    Menu Ex = new Menu ("Extras");
+    for (int i =0; i != extras_list.length; i++)
+      Ex.add(extras_list[i]);
+    
+  }
+
+
 
 
 
